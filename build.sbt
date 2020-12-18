de308bf1e14a00338ac5e6084899a3e57c599c8c
MrPowers <matthewkevinpowers@gmail.com>
2020-12-18 10:17:16 -0500

    More JSON parsing

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ libraryDependencies += "com.lihaoyi" %% "requests" % "0.6.5"
 libraryDependencies += "com.lihaoyi" %% "os-lib" % "0.7.1"
 libraryDependencies += "com.lihaoyi" %% "upickle" % "0.9.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.2" % "test"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
 
 fork in Test := true
 javaOptions ++= Seq("-Xms512M", "-Xmx2048M", "-XX:MaxPermSize=2048M", "-XX:+CMSClassUnloadingEnabled")
--- src/test/scala/mrpowers/scala/design/hands_on/HandsChapter8Spec.scala
+++ src/test/scala/mrpowers/scala/design/hands_on/HandsChapter8Spec.scala
@@ -2,6 +2,7 @@ package mrpowers.scala.design.hands_on
 
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import scala.util.parsing.json._
 
 class HandsChapter8Spec extends AnyFunSpec with Matchers {
 
@@ -59,6 +60,58 @@ class HandsChapter8Spec extends AnyFunSpec with Matchers {
       val brasil = ujson.Obj("population" -> "210 million")
       brasil("cities") = ujson.Arr("recife", "sao paolo")
       os.write(os.pwd/"tmp"/"brasil.json", brasil)
+    }
+
+    it("parses a JSON file") {
+      // https://stackoverflow.com/questions/40172313/scala-read-and-parse-json
+      val jsonString = os.read(os.pwd/"src"/"test"/"resources"/"scala_input.json")
+      val data = ujson.read(jsonString)
+      data("edges_file").str should be("/path/edges.json.gz")
+      data("seed_file").str should be("/path/seed.json.gz")
+    }
+
+    it("parses a JSON string") {
+      val data =
+        """
+{
+   "timestamp":1518501114949,
+   "values":[
+      {
+         "id":"abc",
+         "v":0,
+         "q":true,
+         "t":1518501114487
+      },
+      {
+         "id":"xyz",
+         "v":15,
+         "q":true,
+         "t":1518501114494
+      }
+   ]
+}
+"""
+      val parsed = JSON.parseFull(data)
+      val listAsAny = parsed match {
+        case Some(e:Map[Any,Any]) => e("values")
+        case None => println("Failed.")
+      }
+      val values = listAsAny.asInstanceOf[List[Map[String, Any]]]
+      println(values.map(_("id")))
+    }
+
+  }
+
+  describe("write JSON file") {
+    it("writes an object to JSON") {
+//      val weirdData = ujson.Obj(
+//        "something1" -> ujson.Arr("cat1", "hash1", 101),
+//        "something2" -> ujson.Arr("cat2", "hash2", 102),
+//        "something3" -> ujson.Arr("cat3", "hash3", 103)
+//      )
+//      os.write(os.pwd/"tmp"/"weird_data.json", weirdData)
+val jsonString = os.read(os.pwd/"tmp"/"weird_data.json")
+val data = ujson.read(jsonString)
     }
   }
 
